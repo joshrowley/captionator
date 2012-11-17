82a02bdf5ea07a0843037173417431f4b0541d8d
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-17 14:22:28 -0500

    playing around with twilio, set up sms response

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'twilio-ruby'
+require './credentials'
 
 get '/' do
   erb :index
@@ -9,5 +10,22 @@ end
 class Message
 end
 
-class Twilio
+class TwilioWrapper
+  attr_reader :client
+
+  def initialize
+    @client = Twilio::REST::Client.new $account_sid, $auth_token
+  end
+
+  def send_sms(to)
+    @client.account.sms.messages.create(
+      :from => '+14088004336',
+      :to => to,
+      :body => "Thanks for your text")
+  end
+
+  
 end
+
+twilio = TwilioWrapper.new
+puts twilio.client.inspect
