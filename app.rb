d45a1fbec8caa7157ad3345382fbc01c706351b0
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-24 23:32:50 -0500

    create admin page

--- app.rb
+++ app.rb
@@ -14,10 +14,25 @@ DataMapper.setup(:default, ENV['DATABASE_URL'])
 DataMapper.finalize
 DataMapper.auto_migrate!
 
+helpers do
+
+  def protected!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [ ENV['ZARA_USER'], ENV['ZARA_PASSWORD']]
+  end
+
+end
 
 get '/' do
   Message.save_inbound_messages
-  @messages = Message.all
+  @messages = Message.all(:deleted => false)
   erb :index
 end
 
@@ -31,7 +46,28 @@ get '/update_messages' do
 end
 
 get '/admin' do
-  @messages = Message.all
+  protected!
+  @messages = Message.all(:deleted => false)
+  @deleted_messages = Message.all(:deleted => true)
   erb :admin
 end
 
+get '/delete_message/:id' do
+  protected!
+  Message.get(params[:id]).update(:deleted => true)
+  redirect '/admin'
+end
+
+get '/restore_message/:id' do
+  protected!
+  Message.get(params[:id]).update(:deleted => false)
+  redirect '/admin'
+end
+
+get '/reset_messages' do
+  protected!
+  Message.all.each do |message|
+    message.update(:deleted => true)
+  end
+  redirect '/admin'
+end
